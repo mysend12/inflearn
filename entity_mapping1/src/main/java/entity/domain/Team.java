18c81966a30a84ff73255a09c48eed48defe52ad
mysend12 <mysend12@naver.com>
2020-04-24 06:31:28 +0900

    양방향 연관관계2

--- entity_mapping1/src/main/java/entity/domain/Team.java
+++ entity_mapping1/src/main/java/entity/domain/Team.java
@@ -25,4 +25,5 @@ public class Team {
     // Member의 team변수와 연결되어있다. 1:M에서 뭐랑 연결되어 있는지 적어주기.
     @OneToMany(mappedBy = "team")   
     private List<Member> members = new ArrayList<>();   // 관례상 arrayList로 초기화 해둔다.
+
 }
