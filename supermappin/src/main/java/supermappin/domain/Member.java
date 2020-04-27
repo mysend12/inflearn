2335d46292197b9d6eb67d6fc88d7cb21d7a0169
mysend12 <mysend12@naver.com>
2020-04-28 06:28:14 +0900

    @MappedSuperclass

---
+++ supermappin/src/main/java/supermappin/domain/Member.java
@@ -0,0 +1,20 @@
+package supermappin.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Member extends BaseEntity {
+
+    @Id @GeneratedValue
+    @Column(name = "MEMBER_ID")
+    private Long id;
+    private String username;
+}
